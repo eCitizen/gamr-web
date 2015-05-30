2f72f6234858386a3f75b8070cef7346d6256720
Greg Skiano <gregoryskiano@gmail.com>
2015-05-30 12:12:29 -0400

    better dev env

--- dummyApi.js
+++ dummyApi.js
@@ -9,19 +9,7 @@ router.get('/user', function(req, res, next) {
 });
 
 router.get('/questions', function(req, res, next) {
-  res.json({
-    id: 'testId',
-    sections: [
-      {
-        title: 'Brain Type',
-        instructions: 'Here are Some instructions',
-        scale: 5,
-        questions: [
-          'Are you satisfied?'
-        ]
-      }
-    ]
-  });
+  res.json(require('./stubs/survey.json'));
 });
 
 module.exports = router;
