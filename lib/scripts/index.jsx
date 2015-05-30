78f1cc16b76990a9909a43227e3efae43afcae8b
Greg Skiano <gregoryskiano@gmail.com>
2015-05-30 10:35:01 -0400

    using react with external bundle and jsx transform

---
+++ lib/scripts/index.jsx
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+
+var React = require('react/addons');
+
+React.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('gamr')
+);
+
+(function (){
+  console.log('hello there working 2!');
+}());
