76911d25cadecc7480537656bad7ca7f419a9f49
Greg Skiano <gregoryskiano@gmail.com>
2015-05-30 10:56:08 -0400

    making sure watch reacts to js and jsx

--- lib/scripts/components/Survey.jsx
+++ lib/scripts/components/Survey.jsx
@@ -1,11 +1,12 @@
 /** @jsx React.DOM */
 
 var react = require('react/addons'),
+  api = require('../services/api'),
   Survey;
 
 module.exports = Survey = React.createClass({
   render: function () {
     console.log('hello')
-    return <div>Survey!</div>
+    return <div>A Survey!</div>
   }
 });
