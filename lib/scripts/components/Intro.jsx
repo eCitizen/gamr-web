1ec84344b149e360d498d14dc6dbd45c77922bc7
Greg Skiano <gregoryskiano@gmail.com>
2015-06-08 23:08:33 -0400

    looking at more sketchy ideas

--- lib/scripts/components/Intro.jsx
+++ lib/scripts/components/Intro.jsx
@@ -12,12 +12,16 @@ module.exports = Intro = React.createClass({
       <div id='home'>
         <div className='title-wrap'>
           <div className='extra'></div>
+          <h3 className='small-title'>
+            PROJECT
+          </h3>
           <Title>
             <span>G</span>
             <span>A</span>
             <span>M</span>
             <span>R</span>
           </Title>
+          <h4 className='marquee'>What Does Your Play Style Say About You?</h4>
           <span className='what'>What</span>
           <span className='does'>Does</span>
           <span className='your'>Your</span>
