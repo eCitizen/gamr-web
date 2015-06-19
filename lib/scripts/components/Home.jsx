1cf3ef069f5f4b0ffc23971a328019ed99ce32b8
Greg Skiano <gregoryskiano@gmail.com>
2015-06-18 23:00:46 -0400

    palying with about page

--- lib/scripts/components/Home.jsx
+++ lib/scripts/components/Home.jsx
@@ -5,12 +5,34 @@ var React = require('react/addons'),
   RouteHandler = Router.RouteHandler,
   Nav = require('./Nav.jsx'),
   About = require('./About.jsx'),
+  AboutStore = require('../AboutStore/AboutStore.jsx').Store,
+  classnames = require('classnames'),
   Home;
 
 module.exports = Home = React.createClass({
+  getInitialState: function () {
+    return {
+      about: AboutStore.get()
+    };
+  },
+
+  componentDidMount: function () {
+    AboutStore.addChangeListener(this.handleAbout);
+  },
+
+  componentWillUnount: function () {
+    AboutStore.removeChangeListener(this.handleAbout);
+  },
+
+  handleAbout: function () {
+    this.setState({
+      about: AboutStore.get()
+    });
+  },
+
   render: function () {
     return (
-      <div>
+      <div className={classnames({'about-open': this.state.about})}>
         <Nav/>
         <div className='screen'>
           <RouteHandler {... this.props}/>
