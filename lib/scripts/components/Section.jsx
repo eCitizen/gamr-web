69ef6a97447cd3f1d0e3dc721212ed8618459fd6
Greg Skiano <gregoryskiano@gmail.com>
2015-06-25 22:56:26 -0400

    bringing color back for reminder

--- lib/scripts/components/Section.jsx
+++ lib/scripts/components/Section.jsx
@@ -37,7 +37,28 @@ module.exports.Personality = React.createClass({
   displayName: 'Personality',
   render: function () {
     return (
-      <QuestionSet survey='personality' nextRoute='gamer-type'/>
+      <QuestionSet survey='personality' nextRoute='gamer-type'>
+        <div className='directions push-in'>
+          <p>
+            Describe yourself as you generally are now, not as you wish to be in the future.
+          </p>
+          <p>
+            Describe yourself as you honestly see yourself, in relation to other people you know of the same sex as you are, and roughly your same age.
+          </p>
+        </div>
+        <div className='answerKey'>
+          <p className='flashy'>
+            Indicate for each of the following statements whether it is:
+          </p>
+          <ul className='push-in'>
+            <li><span className='value'>1</span> Very Inaccurate</li>
+            <li><span className='value'>2</span> Moderately Inaccurate</li>
+            <li><span className='value'>3</span> Neither Accurate Nor Inaccurate</li>
+            <li><span className='value'>4</span> Moderately Accurate</li>
+            <li><span className='value'>5</span> Very Accurate</li>
+          </ul>
+        </div>
+      </QuestionSet>
     );
   }
 });
@@ -46,7 +67,28 @@ module.exports.GamerType = React.createClass({
   displayName: 'GamerType',
   render: function () {
     return (
-      <QuestionSet survey='gamerType' nextRoute='reward'/>
+      <QuestionSet survey='gamerType' nextRoute='reward'>
+        <div className='directions push-in'>
+          <p>
+            Describe yourself as you generally are now, not as you wish to be in the future.
+          </p>
+          <p>
+            Describe yourself as you honestly see yourself, in relation to other people you know of the same sex as you are, and roughly your same age.
+          </p>
+        </div>
+        <div className='answerKey'>
+          <p className='flashy'>
+            Indicate for each of the following statements whether it is:
+          </p>
+          <ul className='push-in'>
+            <li><span className='value'>1</span> Very Inaccurate</li>
+            <li><span className='value'>2</span> Moderately Inaccurate</li>
+            <li><span className='value'>3</span> Neither Accurate Nor Inaccurate</li>
+            <li><span className='value'>4</span> Moderately Accurate</li>
+            <li><span className='value'>5</span> Very Accurate</li>
+          </ul>
+        </div>
+      </QuestionSet>
     );
   }
 });
