8b99b5d92f5acba3a9b6cc8fa1b443cd63167c95
Greg Skiano <gregoryskiano@gmail.com>
2015-07-07 22:27:49 -0400

    working on social tools on intro page

--- lib/scripts/components/Intro.jsx
+++ lib/scripts/components/Intro.jsx
@@ -16,13 +16,20 @@ module.exports = React.createClass({
         </div>
         <h4 className='marquee'>What does your play style say about you?</h4>
         <h4 className='marquee b'>Join us, and find out in just 15 minutes.</h4>
-        <Link to="consent" className=''><Button>Start</Button></Link>
-        <div className='intro-social'>
-          <i className='fa fa-twitter'/>
-          <i className='fa fa-facebook-official'/>
-          <span className='intro-pipe'>|</span>
-          <span className='intro-about'>About</span>
+
+        <div className='center-group intro-actions'>
+          <Link className='middle' to="consent">
+            <Button>Start</Button>
+          </Link>
+          <div className='left intro-about'>
+            <span>About Us</span>
+          </div>
+          <div className='right intro-social'>
+            <i className='fa fa-twitter'/>
+            <i className='fa fa-facebook-official'/>
+          </div>
         </div>
+
         <p className='schools'>MIT Media Lab <span className='plus'>+</span> Tilburg University</p>
         <p className='footnote'>
           To participate, you must have an account with one or more of the folllowing:<br/> League of Legends, Battlefield: Hardline, or World of Warcraft.
