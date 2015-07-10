245c0833885e0bd7412ef1f0fc6bc076467d8bf8
Greg Skiano <gregoryskiano@gmail.com>
2015-07-09 23:15:20 -0400

    getting the confirmation to look more decent

--- lib/scripts/components/IdentityResults.jsx
+++ lib/scripts/components/IdentityResults.jsx
@@ -2,6 +2,7 @@
 var React = require('react/addons');
 var Link = require('react-router').Link;
 var Button = require('./Button.jsx');
+var Title = require('./Title.jsx');
 
 module.exports = React.createClass({
   displayName: 'IdentityResults',
@@ -19,15 +20,19 @@ module.exports = React.createClass({
         <div className='inner'>
           <ul className='profile-results'>
             <li>
-              <h4>World of Warcraft</h4>
+              <Title className='result-title'>World of Warcraft</Title>
+              <h6>Player Name</h6>
+            </li>
+            <li>
+              <Title className='result-title'>Battlefield: Hardline</Title>
               <h6>Player Name</h6>
             </li>
           </ul>
-          <Link to="bio">
+          <Link className='results-confirm' to="bio">
             <Button>Looks Good</Button>
           </Link>
-          <p>
-            Something missing? <Link to='games'>Change your information</Link>
+          <p className='small-text'>
+            Something missing? <Link className='results-change' to='games'>Change your information</Link>
           </p>
         </div>
       </div>
