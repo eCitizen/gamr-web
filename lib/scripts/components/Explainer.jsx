34c2689c60d0715b89655e21ea2670156ce5811c
Greg Skiano <gregoryskiano@gmail.com>
2015-07-20 22:34:20 -0400

    moving social

--- lib/scripts/components/Explainer.jsx
+++ lib/scripts/components/Explainer.jsx
@@ -11,33 +11,41 @@ module.exports = React.createClass({
     return (
       <div className='intro-body'>
         <div className='intro-body-inner'>
+          
+
+          <h4>
+            What does your play style say about you?
+          </h4>
+
+          {/*
           <div className='intro-social'>
             <i className='fa fa-twitter'/>
             <i className='fa fa-facebook-official'/>
           </div>
-
-          <p>
-            What does your play style say about you?
-          </p>
+          */}
 
           <Video/>
 
           <p>
-            The Playful Systems group at MIT Media Lab has teamed up with Tilburg University to study how personality relates to gameplay.
+            The Playful Systems group at <strong>MIT Media Lab</strong> has teamed up with <strong>Tilburg University</strong> to study how personality relates to gameplay.
           </p>
           <p>
-            We are asking players to participate in a three-part survey about their personality, brain type, and gamer type. If you play one or more of the following, you may be elligible!
+            We are asking players to participate in a three-part survey about their personality, brain type, and gamer type. <em>If you play one or more of the following, you may be elligible!</em>
           </p>
 
-          <p className='schools'>MIT Media Lab <span className='plus'>+</span> Tilburg University</p>
+          <p className='schools'>League of Legends</p>
+          <p className='schools'>Battlefield: Hardline</p>
+          <p className='schools'>Battlefield 4</p>
+          <p className='schools'>World of Warcraft</p>
 
           <Link className='start-button' to="consent">
             <Button>Start</Button>
           </Link>
-          
-          <p className='footnote'>
-            To participate, you must play one or more of the folllowing:<br/> League of Legends, Battlefield: Hardline, Battlefield 4, or World of Warcraft.
-          </p>
+
+          <div className='intro-social'>
+            <i className='fa fa-twitter'/>
+            <i className='fa fa-facebook-official'/>
+          </div>
         </div>
       </div>
     );
