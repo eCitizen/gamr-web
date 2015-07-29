f7b13261d5338cbab97ed518fe64c6559d76c282
Greg Skiano <gregoryskiano@gmail.com>
2015-07-28 22:29:47 -0400

    adding transition group for error

--- lib/scripts/components/Error.jsx
+++ lib/scripts/components/Error.jsx
@@ -1,5 +1,6 @@
 
 var React = require('react');
+var CSSTransitionGroup = React.addons.CSSTransitionGroup;
 var errorSvc = require('../services/error');
 
 module.exports = React.createClass({
@@ -35,9 +36,10 @@ module.exports = React.createClass({
   },
 
   render: function () {
+    var child = <div/>;
     if (this.state.error) {
-      return (
-        <div className='gamr-error-wrap'>
+      child = (
+        <div key='message' className='gamr-error-wrap'>
           <div className='gamr-error' onClick={errorSvc.dismiss}>
             <h4>{this.state.error.title}</h4>
             <p>{this.state.error.message}</p>
@@ -45,8 +47,11 @@ module.exports = React.createClass({
           </div>
         </div>
       );
-    } else {
-      return null;
     }
+    return (
+      <CSSTransitionGroup transitionName='err-fade'>
+        {child}
+      </CSSTransitionGroup>
+    );
   }
 });
