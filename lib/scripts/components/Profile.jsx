41c0f215265fc146744e73a81c096f1ea54edddb
Greg Skiano <gregoryskiano@gmail.com>
2015-08-03 21:31:39 -0400

    putting in some logging to see what is going on with mutations

--- lib/scripts/components/Profile.jsx
+++ lib/scripts/components/Profile.jsx
@@ -13,7 +13,9 @@ module.exports = React.createClass({
 
   getInitialState: function () {
 
-    surveyStore.getScores();
+    // surveyStore.getScores();
+    // surveyStore.getProfile();
+    surveyStore.getProfileHash();
 
     return {
       processed: false,
