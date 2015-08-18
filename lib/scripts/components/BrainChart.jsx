884e3be0c69cad44b8024955899d386516b45bf2
Greg Skiano <gregoryskiano@gmail.com>
2015-08-18 01:04:58 -0400

    verifying i can plot on the chart

--- lib/scripts/components/BrainChart.jsx
+++ lib/scripts/components/BrainChart.jsx
@@ -7,21 +7,28 @@ module.exports = React.createClass({
 
   componentDidMount: function () {
 
-    var EQ = this.props.Empathizing.raw;
-    var SQ = this.props.Systemizing.raw;
-    
-    console.log(EQ, SQ);
+    console.log(this.props)
+
+    var data = [
+      {y: 12, x: 26},
+      {y: 32, x: 24},
+      {y: 6,  x: 13},
+      {y: 16, x: 12},
+      {y: 32, x: 12},
+      {y: 32, x: 0},
+      {y: 2, x: 25},
+      {x: this.props.Systemizing, y: this.props.Empathizing}
+    ];
 
     var wrapper = d3.select(React.findDOMNode(this));
 
-    // add the tooltip area to the webpage
     var tooltip = d3.select("body").append("div")
       .attr("class", "tooltip")
       .style("opacity", 0);
 
-    var margin = {top: 10, right: 10, bottom: 25, left: 25},
-      width = 225 - margin.left - margin.right,
-      height = 225 - margin.top - margin.bottom;
+    var margin = {top: 10, right: 10, bottom: 25, left: 25};
+    var width = 225 - margin.left - margin.right;
+    var height = 225 - margin.top - margin.bottom;
 
     var x = d3.scale.linear()
       .range([width, 0]);
@@ -31,111 +38,84 @@ module.exports = React.createClass({
 
     var xAxis = d3.svg.axis()
       .scale(x)
-      // .tickValues([33.333,66.666])
       .orient("bottom");
 
     var yAxis = d3.svg.axis()
       .scale(y)
-      // .tickValues([33.333,66.666])
       .orient("left");
 
+    x.domain([brainTypeHelper.SQ_MIN, brainTypeHelper.SQ_MAX].reverse());
+    y.domain([brainTypeHelper.EQ_MIN, brainTypeHelper.EQ_MAX]);
 
     var svg = wrapper.append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
+        .attr("class", "chart-wrap")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // backgrround
-    // http://stackoverflow.com/questions/17776641/fill-rect-with-pattern
+    definePatterns(svg);
 
-    // see this....
-    // http://stackoverflow.com/questions/13069446/simple-fill-pattern-in-svg-diagonal-hatching
+    var background = svg.append("g").attr('class', 'chart-background');
 
-    var defs =  svg.append('defs');
+    shadeRegions(background, brainTypeHelper.makeThresholdLineFunctions());
 
-    defs.append('pattern')
-        .attr('id', 'diagonalHatch')
-        .attr('patternUnits', 'userSpaceOnUse')
-        .attr('width', 6)
-        .attr('height', 4)
-      .append('path')
-        .attr('d', 'M0,4 l0,-4')
-        .attr('stroke', '#fff')
-        .attr('stroke-width', 2);
+    // function makeLine(threshold) {
+    //   var line = d3.svg.line()
+    //     .x(function(d) { return x(d.x); })
+    //     .y(function(d) { return y(d.y); });
 
-    defs.append('defs')
-      .append('pattern')
-        .attr('id', 'horizontalHatch')
-        .attr('patternUnits', 'userSpaceOnUse')
-        .attr('width', 4)
-        .attr('height', 6)
-      .append('path')
-        .attr('d', 'M4,0 l-4,-0')
-        .attr('stroke', '#fff')
-        .attr('stroke-width', 2);
+    //   points = [
+    //      {x: 0, y: threshold},
+    //      {x: 100, y: threshold + 100},
+    //   ]
 
-    // end background
+    //   svg.append("path")
+    //     .datum(points)
+    //     .attr("class", "line")
+    //     .attr("d", line);
+    // }
 
-    x.domain([100, 0]);
-    y.domain([0, 100]);
+    // function makeArea(upper, lower, opacity, className) {
+    //   className = className || 'up';
+    //   function getPoint(x) { 
+    //     return {x: x, y: x + upper}
+    //   }
 
-    function makeLine(threshold) {
-      var line = d3.svg.line()
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); });
+    //   var area = d3.svg.area()
+    //     .x(function(d) { return x(d.x); })
+    //     .y1(function(d) { return y(d.y); })
+    //     .y0(function(d) { 
+    //       return y(d.y - (upper - lower)); 
+    //     });
 
-      points = [
-         {x: 0, y: threshold},
-         {x: 100, y: threshold + 100},
-      ]
+    //   points = [
+    //     getPoint(0),
+    //     // getPoint(0 - upper),
+    //     // getPoint(100 - upper),
+    //     getPoint(100)
+    //   ]
 
-      svg.append("path")
-        .datum(points)
-        .attr("class", "line")
-        .attr("d", line);
-    }
+    //   // console.log(points);
 
-    function makeArea(upper, lower, opacity, className) {
-      className = className || 'up';
-      function getPoint(x) { 
-        return {x: x, y: x + upper}
-      }
+    //   svg.append("path")
+    //     .datum(points)
+    //     .attr("class", "area " + className)
+    //     .style('fill-opacity', opacity)
+    //     .attr("d", area);
+    // }
 
-      var area = d3.svg.area()
-        .x(function(d) { return x(d.x); })
-        .y1(function(d) { return y(d.y); })
-        .y0(function(d) { 
-          return y(d.y - (upper - lower)); 
-        });
-
-      points = [
-        getPoint(0),
-        // getPoint(0 - upper),
-        // getPoint(100 - upper),
-        getPoint(100)
-      ]
-
-      // console.log(points);
-
-      svg.append("path")
-        .datum(points)
-        .attr("class", "area " + className)
-        .style('fill-opacity', opacity)
-        .attr("d", area);
-    }
-
-    makeArea(100 * 2 / 3, 100, .3);
-    makeArea(100/3, 100*2/3, .2);
-    // makeArea(-100/3, 100/3, .1);
-    makeArea(-100*2/3, -100/3, .2, 'right');
-    makeArea(-100, -100*2/3, .3, 'right');
+    // makeArea(100 * 2 / 3, 100, .3);
+    // makeArea(100/3, 100*2/3, .2);
+    // // makeArea(-100/3, 100/3, .1);
+    // makeArea(-100*2/3, -100/3, .2, 'right');
+    // makeArea(-100, -100*2/3, .3, 'right');
 
     // makeLine(0);
-    makeLine(100/3);
-    makeLine(100*2/3);
-    makeLine(-100/3);
-    makeLine(-100*2/3);
+    // makeLine(100/3);
+    // makeLine(100*2/3);
+    // makeLine(-100/3);
+    // makeLine(-100*2/3);
 
     svg.append("g")
       .attr("class", "x axis")
@@ -161,14 +141,7 @@ module.exports = React.createClass({
       .text("Empathizing");
 
 
-    var dots = svg.selectAll(".dot")
-      .data([
-        // {x: 10, y: 10},
-        // {x: 50, y: 50},
-        // {x: 100, y: 100},
-        // {x: 0, y: 100},
-        {x: 23, y: 78}
-      ])
+    var dots = svg.selectAll(".dot").data(data);
 
     dots.enter().append("circle")
       .attr("class", "dot")
@@ -186,7 +159,7 @@ module.exports = React.createClass({
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html("hello")
+          tooltip.html(brainTypeHelper.getBrainType(d.x, d.y))
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
       })
@@ -195,6 +168,28 @@ module.exports = React.createClass({
                .duration(500)
                .style("opacity", 0);
       });
+    function shadeRegions(svg, fns) {
+      console.log(fns);
+      fns.forEach(function (thresholdFn, idx) {
+        makeLine(svg, thresholdFn);
+      });
+    }
+
+    function makeLine(svg, thresholdFn) {
+      var line = d3.svg.line()
+        .x(function(d) { return x(d.x); })
+        .y(function(d) { return y(d.y); });
+
+      points = [
+        {x: brainTypeHelper.SQ_MIN, y: thresholdFn(brainTypeHelper.SQ_MIN)},
+        {x: brainTypeHelper.SQ_MAX, y: thresholdFn(brainTypeHelper.SQ_MAX)}
+      ];
+
+      svg.append("path")
+        .datum(points)
+        .attr("class", "line")
+        .attr("d", line);
+    }
   },
 
   render: function () {
@@ -204,4 +199,34 @@ module.exports = React.createClass({
 
 
 
+function definePatterns(svg) {
+  // backgrround
+  // http://stackoverflow.com/questions/17776641/fill-rect-with-pattern
+
+  // see this....
+  // http://stackoverflow.com/questions/13069446/simple-fill-pattern-in-svg-diagonal-hatching
+
+  var defs = svg.append('defs');
+
+  defs.append('pattern')
+      .attr('id', 'diagonalHatch')
+      .attr('patternUnits', 'userSpaceOnUse')
+      .attr('width', 6)
+      .attr('height', 4)
+    .append('path')
+      .attr('d', 'M0,4 l0,-4')
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 2);
+
+  defs.append('defs')
+    .append('pattern')
+      .attr('id', 'horizontalHatch')
+      .attr('patternUnits', 'userSpaceOnUse')
+      .attr('width', 4)
+      .attr('height', 6)
+    .append('path')
+      .attr('d', 'M4,0 l-4,-0')
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 2);
+}
 
